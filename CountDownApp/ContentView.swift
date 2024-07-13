57b947489d518ec15fe745b392b0e5f8eeff2c96
EmiriNakamori <emiri060414@gmail.com>
2024-07-13 17:13:28 +0900

    Initial Commit

---
+++ CountDownApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CountDownApp
+//
+//  Created by nakamori.emiri on 2024/07/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CountDownApp/CountDownAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownAppApp.swift
+//  CountDownApp
+//
+//  Created by nakamori.emiri on 2024/07/13.
+//
+
+import SwiftUI
+
+@main
+struct CountDownAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
